9d08b351d3ef478d315f4ac43fe7d57761e8c36e
petrostorosyan <petrostorosyan23@gmail.com>
2024-09-02 12:13:51 +0400

    make animation box

--- src/components/main/menu-section/index.tsx
+++ src/components/main/menu-section/index.tsx
@@ -6,43 +6,46 @@ import lines from "../../../../public/assets/images/decores/straight-lines.png";
 import salad from "../../../../public/assets/images/home-page/menu/salad.webp";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const MenuAndPrivateDining = () => {
   gsap.registerPlugin(ScrollTrigger);
 
   const infoRef = useRef<HTMLDivElement | null>(null);
 
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: `.${styles.infoBox}`,
-      scrub: true,
-    //   markers: true,
-      start: "50% 60%",
-      end: "55% 55%",
-    },
-  });
+  useEffect(() => {
+    if (infoRef.current) {
+      const infoBoxElement = infoRef.current;
 
-  useGSAP(
-    () => {
-      if (infoRef.current) {
-        tl.from(`.${styles.infoBox}`, {
-          x: 1930,
-          duration: 2,          
-        })
-        // .to(`.${styles.infoBox}`, {x: 930});
-      }
-    },
-    { scope: infoRef }
-  );
+      gsap.fromTo(
+        infoBoxElement,
+        {
+          x: window.innerWidth, // Start off-screen to the right
+          // opacity: 0, // Optional: start with opacity 0 for a fade-in effect
+        },
+        {
+          x: 940, // End at 1100 pixels from the left
+          // opacity: 1, // Fade to full opacity
+          duration: 2,
+          ease: "power3.out", // Smooth easing
+          scrollTrigger: {
+            trigger: infoBoxElement,
+            start: "top 75%", // Start animation when the top of the element is 75% down the viewport
+            end: "top 50%", // Adjust this value as needed
+            scrub: true, // Sync animation with scrolling
+            // markers: true, // Uncomment for debugging
+          },
+        }
+      );
+    }
+  }, []);
 
   return (
     <div className={styles.menuContainer}>
-      <div ref={infoRef} className={styles.menuContent}>
+      <div className={styles.menuContent}>
         <Image className={styles.image} src={salad} alt={"salad"} />
         <Image className={styles.decor} src={lines} alt={"decor"} />
-        <div className={styles.infoBox}>
+        <div ref={infoRef} className={styles.infoBox}>
           <p>title</p>
           <p>description</p>
           <button>view menu</button>
